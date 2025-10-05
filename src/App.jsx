a08e5d60631e70ef451e8fa6855a33503bbb1649
= <=>
2025-10-05 01:31:30 -0700

    create pages with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
  import React from 'react'
+
+ import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Projects from './pages/Projects'
+import DashBoard from './pages/DashBoard'
+import Header from './components/Header'
+
  
  const App = () => {
    return (
-     <div className=''>App</div>
+     <BrowserRouter>
+     <Header/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/login' element ={<Login/>}/>
+      <Route path='/signup' element ={<SignUp/>}/>
+      <Route path='/projects' element ={ <Projects/>}/>
+      <Route path='/dashboard' element ={<DashBoard/>}/>
+     </Routes>
+     </BrowserRouter>
    )
  }
  
